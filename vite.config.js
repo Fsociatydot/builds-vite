4f219a131a276fe49bc6ab2018e25acaf5228374
Amberits <info@hdsteam.com>
2024-03-10 12:50:34 +0300

    stylint-prettier

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,41 @@
-import checker from 'vite-plugin-checker'
-import VitePluginBrowserSync from 'vite-plugin-browser-sync'
-import path from 'path'
-import { qrcode } from 'vite-plugin-qrcode'
-
-console.log(path.resolve(__dirname, './src/assets'))
+import checker from "vite-plugin-checker"
+import VitePluginBrowserSync from "vite-plugin-browser-sync"
+import path from "path"
+import { qrcode } from "vite-plugin-qrcode"
 
 export default {
   plugins: [
-   
     checker({
       typescript: true,
-       eslint: {
+      eslint: {
         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
       },
       stylelint: {
-        lintCommand: 'stylelint ./src/**/*.{css,scss}'
+        lintCommand: "stylelint ./src/**/*.{css,scss} --fix",
       },
     }),
-     VitePluginBrowserSync(),
+    VitePluginBrowserSync(),
     qrcode(),
   ],
   resolve: {
     alias: {
-        '@': path.resolve(__dirname, './src/assets')
-    }
+      "@": path.resolve(__dirname, "./src/assets"),
+    },
   },
   Host: {
     rollupOptions: {
-        input: {
-            main: path.resolve(__dirname, 'index.html'),
-            login: path.resolve(__dirname, 'login/index.html'),
-        }
-    }
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "login/index.html"),
+      },
+    },
   },
   Build: {
     rollupOptions: {
-        input: {
-            main: path.resolve(__dirname, 'index.html'),
-            login: path.resolve(__dirname, 'login/index.html'),
-        }
-    }
-  }
-
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "login/index.html"),
+      },
+    },
+  },
 }
