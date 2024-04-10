ac1c70b19d04463c36d25653af0717059fa6880c
denial <277@sfx-tula.ru>
2024-04-10 11:09:25 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import checker from "vite-plugin-checker"
 import VitePluginBrowserSync from "vite-plugin-browser-sync"
 import path from "path"
 import { qrcode } from "vite-plugin-qrcode"
+import handlebars from "vite-plugin-handlebars"
+import EnvironmentPlugin from "vite-plugin-environment"
 
 export default {
   plugins: [
@@ -11,11 +13,18 @@ export default {
         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
       },
       stylelint: {
-        lintCommand: "stylelint ./src/**/*.{css,scss} --fix",
+        lintCommand: "stylelint ./src/**/**/*.{css,scss}  --fix",
       },
     }),
+    EnvironmentPlugin("all"),
     VitePluginBrowserSync(),
     qrcode(),
+    handlebars({
+      context: {
+        title: "Техноберинг",
+      },
+      partialDirectory: path.resolve(__dirname, "./src/assets/partials"),
+    }),
   ],
   resolve: {
     alias: {
@@ -26,7 +35,9 @@ export default {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
+        product: path.resolve(__dirname, "product/index.html"),
         login: path.resolve(__dirname, "login/index.html"),
+        uikit: path.resolve(__dirname, "uikit/index.html"),
       },
     },
   },
@@ -34,7 +45,27 @@ export default {
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
+        product: path.resolve(__dirname, "product/index.html"),
         login: path.resolve(__dirname, "login/index.html"),
+        uikit: path.resolve(__dirname, "uikit/index.html"),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1)
+          if (/png|jpe?g|svg|gif|tiff|bmp|webp|ico/i.test(extType)) {
+            console.log(assetInfo)
+            extType = "img"
+          } else if (/ttf|woff|woff2/i.test(extType)) {
+            extType = "fonts"
+          }
+          return `assets/${extType}/[name][extname]`
+        },
+        chunkFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/[name].js",
       },
     },
   },
